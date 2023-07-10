b588a75584aaa64a812d36bf480426a95dda5331
Eshuigugu <65567073+Eshuigugu@users.noreply.github.com>
2023-07-10 07:25:56 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import time
 import requests
 import json
-from bs4 import BeautifulSoup
 import os
 import pickle
 from appdirs import user_data_dir
+import html
+
 
 # this script does create some files under this directory
 appname = "search_cloudlibrary"
@@ -35,36 +36,38 @@ else:
 
 
 def search_cloudlibrary(title, authors, mediatype):
-    queries = list({f'{title} {author}'.replace('&', 'and')  # some characters such as & and # end the search string prematurely
-                    for author in authors[:2]})[:20]  # search by title + series and author, max of 20 queries
+    # some characters such as & and # end the search string prematurely
+    queries = list({f'{title} {author}'.replace('&', 'and')
+                    for author in authors[:2]})[:20]  # search by title, max of 20 queries
     media_items = []
     for library_name in cloudlibrary_urlNames:
-        api_url = f'https://ebook.yourcloudlibrary.com/uisvc/{library_name}/Search/CatalogSearch'
+        api_url = f'https://ebook.yourcloudlibrary.com/library/{library_name}/search'
         for query in queries:
             params = {
-                'media': mediatype,
-                'src': 'lib',
-            }
-            json_payload = {
-                "SearchString": query,
-                "SortBy": "publication_date",  # sort new > old
-                "Segment": 1,
-                "SegmentSize": 5
+                'format': mediatype,
+                'available': 'true',
+                'language': '',
+                'sort': '',
+                'segment': '1',
+                'orderBy': 'relevence',
+                'owned': 'yes',
+                'query': query,
+                '_data': 'routes/library/$name/search',
             }
             try:
-                r = sess.post(api_url, params=params, json=json_payload, timeout=30)
+                r = sess.get(api_url, params=params, timeout=30)
             except (requests.ConnectionError, requests.exceptions.ReadTimeout) as e:
                 print(f'error {e}')
                 time.sleep(10)
                 continue
             time.sleep(1)
             r_json = r.json()
-            if r.status_code == 200 and r_json['Items']:
-                for media_item in r_json['Items']:
+            if r.status_code == 200 and 'items' in r_json['results']['search'] and r_json['results']['search']['items']:
+                for media_item in r_json['results']['search']['items']:
                     for k, v in list(media_item.items()):
                         media_item[k.lower()] = v
                     media_item['url'] = f'https://ebook.yourcloudlibrary.com/library/{library_name}/Featured/ItemDetail/{media_item["id"]}'
-                media_items += r_json['Items']
+                media_items += r_json['results']['search']['items']
     # ensure each result is unique
     media_items = list({x['url']: x for x in media_items}.values())
     return media_items
@@ -81,7 +84,7 @@ def get_mam_requests(limit=5000):
         url = 'https://www.myanonamouse.net/tor/json/loadRequests.php'
         headers = {}
         # fill in mam_id for first run
-        headers['cookie'] = 'mam_id='
+        # headers['cookie'] = 'mam_id='
 
         query_params = {
             'tor[text]': '',
@@ -113,37 +116,51 @@ def get_mam_requests(limit=5000):
             f.write(str(book['id']) + '\n')
             book['url'] = 'https://www.myanonamouse.net/tor/viewRequest.php/' + \
                           str(book['id'])[:-5] + '.' + str(book['id'])[-5:]
-            book['title'] = BeautifulSoup(book["title"], features="lxml").text
+            book['title'] = html.unescape(str(book['title']))
             if book['authors']:
                 book['authors'] = [author for k, author in json.loads(book['authors']).items()]
     return req_books
 
 
+def should_search_for_book(mam_book):
+    # category 79 is magazines/newspapers
+    return (mam_book['cat_name'].startswith('Ebooks ') or mam_book['cat_name'].startswith('Audiobooks '))\
+           and mam_book['filled'] == 0\
+           and mam_book['torsatch'] == 0\
+           and mam_book['category'] != 79\
+           and mam_book['id'] not in blacklist
+
+
+def search_for_mam_book(book):
+    mediatype = book['cat_name'].split(' ')[0][:5].lower()  # will be ebook or audio
+    try:
+        return search_cloudlibrary(book['title'], book['authors'], mediatype)
+    except Exception as e:
+        print('error', e)
+        return
+
+
+def pretty_print_hits(mam_book, hits):
+    print(mam_book['title'])
+    print(' ' * 2 + mam_book['url'])
+    if len(hits) > 5:
+        print(' ' * 2 + f'got {len(hits)} hits')
+        print(' ' * 2 + f'showing first 5 results')
+        hits = hits[:5]
+    for hit in hits:
+        print(' ' * 2 + hit["title"])
+        print(' ' * 4 + hit['url'])
+    print()
+
+
 def main():
     req_books = get_mam_requests()
-
-    req_books_reduced = [x for x in req_books if
-                         (x['cat_name'].startswith('Ebooks ') or x['cat_name'].startswith('Audiobooks '))
-                         and x['filled'] == 0
-                         and x['torsatch'] == 0
-                         and x['category'] != 79  # remove magazines/newspapers
-                         and x['id'] not in blacklist]
-    for book in req_books_reduced:
-        mediatype = book['cat_name'].split(' ')[0][:5].lower()  # will be ebook or audio
-        hits = []
-        hits += search_cloudlibrary(book['title'], book['authors'], mediatype)
+    for book in filter(should_search_for_book, req_books):
+        hits = search_for_mam_book(book)
         if hits:
-            print(book['title'])
-            print(' ' * 2 + book['url'])
-            if len(hits) > 5:
-                print(' ' * 2 + f'got {len(hits)} hits')
-                print(' ' * 2 + f'showing first 5 results')
-                hits = hits[:5]
-            for hit in hits:
-                print(' ' * 2 + hit["title"])
-                print(' ' * 4 + hit['url'])
-            print()
+            pretty_print_hits(book, hits)
 
 
 if __name__ == '__main__':
     main()
+
